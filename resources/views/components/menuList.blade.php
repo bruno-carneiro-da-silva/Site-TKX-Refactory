e032aafc3f5a7bf22d79ee4f0eb45ecc39e27ce2
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-09-14 12:49:25 -0300

    :construction: feat: changing routes

--- resources/views/components/menuList.blade.php
+++ resources/views/components/menuList.blade.php
@@ -1,37 +1,9 @@
 <nav class="main-nav">
     <ul class="parent-nav">
         <li>
-            <a class="nav-link " href="#">
+            <a class="nav-link " href="{{ url('/') }}">
                 <span class="rolling-text">PÁGINA INICIAL</span>
             </a>
-            {{-- <ul class="submenu mega-menu-wrapper">
-                <li class="menu-single-list">
-                    <a href="#" class="top-title">Multipages</a>
-                    <ul>
-                        <li><a href="{{ route('index') }}">Corporate Demo</a></li>
-                        <li><a href="{{ route('index-two') }}">Financial Demo</a></li>
-                        <li><a href="{{ route('index-three') }}">Marketing Demo</a></li>
-                        <li><a href="{{ route('index-four') }}">Agency Demo</a></li>
-                        <li><a href="{{ route('index-five') }}">Startup Demo</a></li>
-                        <li><a href="{{ route('index-six') }}">Construction Demo</a></li>
-                        <li><a href="{{ route('index-seven') }}">Construction 2 Demo</a></li>
-                        <li><a href="{{ route('index-eight') }}">Company Demo</a></li>
-                    </ul>
-                </li>
-                <li class="menu-single-list">
-                    <a href="#" class="top-title">OnePage</a>
-                    <ul>
-                        <li><a href="{{ route('onepage-one') }}">Onepage Corporate</a></li>
-                        <li><a href="{{ route('onepage-two') }}">Onepage Financial</a></li>
-                        <li><a href="{{ route('onepage-three') }}">Onepage Marketing</a></li>
-                        <li><a href="{{ route('onepage-four') }}">Onepage Agency</a></li>
-                        <li><a href="{{ route('onepage-five') }}">Onepage Startup</a></li>
-                        <li><a href="{{ route('onepage-six') }}">Onepage Construction</a></li>
-                        <li><a href="{{ route('onepage-seven') }}">Onepage Construction 2</a></li>
-                        <li><a href="{{ route('onepage-eight') }}">Onepage Company</a></li>
-                    </ul>
-                </li>
-            </ul> --}}
         </li>
         <li>
             <a class="nav-link" href="{{ route('about') }}">
@@ -52,7 +24,6 @@
                 <span class="rolling-text">BLOG</span>
             </a>
             <ul class="submenu">
-                <li><a href="{{ route('blog') }}">Blog</a></li>
                 <li><a href="{{ route('blog-col-1') }}">Blog Col 1</a></li>
                 <li><a href="{{ route('blog-col-2') }}">Blog Col 2</a></li>
                 <li><a href="{{ route('blog-details') }}">Blog </a></li>
@@ -69,8 +40,8 @@
                 <span class="rolling-text">ÁREA DO CLIENTE</span>
             </a>
             <ul class="submenu">
-                <li><a href="{{ route('appoinment') }}">Appoinment</a></li>
-                <li><a href="{{ route('projects-details-2') }}">Project Details v2</a></li>
+                <li><a href="{{ route('appoinment') }}">Serviço Personalizado</a></li>
+                <li><a href="{{ route('projects-details-2') }}">Detalhes do Projeto v2</a></li>
                 <li><a href="{{ route('team') }}">Equipe</a></li>
                 <li><a href="{{ route('team-details') }}">Detalhes da Equipe</a></li>
                 <li><a href="{{ route('pricing') }}">Preços</a></li>
